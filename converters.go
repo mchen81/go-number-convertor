b9fba8240f075655734b0c063198ef52e70a0cbe
Jerry <mchen81@dons.usfca.edu>
2021-07-05 19:54:24 +0800

    finished

---
+++ converters.go
@@ -0,0 +1,38 @@
+package main
+
+import "strconv"
+
+func toBinary(number uint32) string {
+	return strconv.FormatInt(int64(number), 2)
+}
+
+func toHumanReadBinary(number uint32) string {
+
+	var chars [39]byte
+	var m byte
+	counter := 0
+	for i := range chars {
+
+		if counter == 4 {
+			chars[38-i] = ' '
+			counter = 0
+			continue
+		}
+
+		if number > 0 {
+			m = byte((number % 2) + '0')
+			number /= 2
+			chars[38-i] = m
+		} else {
+			chars[38-i] = '0'
+		}
+
+		counter++
+	}
+
+	return string(chars[:])
+}
+
+func toHex(number uint32) string {
+	return strconv.FormatInt(int64(number), 16)
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
@@ -17,20 +18,23 @@ import (
 // nt -u (base10 unsigned number)
 
 func main() {
-	number := parseArg()
-	fmt.Println(number)
 
-	// output:
-	// baseed 10 sign
-	// based 10 unsign
-	// binary (humand read)
-	// hex
+	number := parseArgToInt()
+	un := uint32(number)
+	bi := toBinary(un)
+	bih := toHumanReadBinary(un)
+	hex := toHex(un)
+
+	fmt.Printf("(signed) %d\n", number)
+	fmt.Printf("(unsigned) %d\n", un)
+	fmt.Printf("(binary) 0b%s\n", bi)
+	fmt.Printf("(binary) %s\n", bih)
+	fmt.Printf("(unsigned) 0x%s\n", hex)
 
 }
 
-func parseArg() int64 {
+func parseArgToInt() int32 {
 	args := os.Args[1:]
-	fmt.Println(args)
 	argsLength := len(args)
 
 	// arg types
@@ -39,38 +43,55 @@ func parseArg() int64 {
 	// case1 a number
 	if argsLength < 1 {
 		log.Fatal("No Args Found")
-	} else if argsLength == 1 {
-		var tmpInt int
-		tmpInt, err = strconv.Atoi(args[0])
+	}
+	inputNumber := args[0]
+	if len(inputNumber) < 2 {
+		number, err = strconv.ParseInt(inputNumber, 10, 32)
 		if err != nil {
-			log.Fatalf("%s is not a digital number.", args[1])
+			log.Fatalf("%s is not a valid binary number", inputNumber)
 		}
-		number = int64(tmpInt)
+		return int32(number)
+	}
+
+	prefix := inputNumber[0:2]
+
+	if prefix == "0b" {
+		// binary
+		inputNumber = inputNumber[2:]
+		number, err = strconv.ParseInt(inputNumber, 2, 64)
+		if err != nil {
+			log.Fatalf("%s is not a valid binary number", inputNumber)
+		}
+	} else if prefix == "0x" {
+		// hex
+		number, err = strconv.ParseInt(inputNumber, 16, 64)
+		if err != nil {
+			log.Fatalf("%s is not a valid hex number", inputNumber)
+		}
 	} else {
-		inputType := args[0]
-		inputNumber := args[1]
-		switch inputType {
-		case "-b":
-			number, err = strconv.ParseInt(inputNumber, 2, 64)
-			if err != nil {
-				log.Fatalf("%s is not a valid binary number", inputNumber)
-			}
-		case "-u":
+		// base10
+		// unsign
+		const MaxUint = ^uint(0)
+		const MinUint = 0
+		const MaxInt = int(MaxUint >> 1)
+		const MinInt = -MaxInt - 1
+
+		number, err = strconv.ParseInt(inputNumber, 10, 64)
+		if err != nil {
+			log.Fatalf("not support %s", inputNumber)
+		}
+		if number <= math.MaxInt32 && number >= math.MinInt32 {
+			return int32(number)
+		} else if number > math.MaxInt32 && number <= math.MaxUint32 {
 			var ui uint64
 			ui, err = strconv.ParseUint(inputNumber, 10, 64)
 			if err != nil {
 				log.Fatalf("%s is not a valid unsign int", inputNumber)
 			}
 			number = int64(ui)
-		case "-x":
-			number, err = strconv.ParseInt(inputNumber, 16, 64)
-			if err != nil {
-				log.Fatalf("%s is not a valid hex number", inputNumber)
-			}
-		default:
-			log.Fatalf("Cannot understand the input type: %s", inputType)
+		} else {
+			log.Fatalf("not support %s", inputNumber)
 		}
-
 	}
-	return number
+	return int32(number)
 }
